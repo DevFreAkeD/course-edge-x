f42000234ed968bc758e1e49f7b5be0ff5d15904
Tushar Nagar <tusharrock146@gmail.com>
2025-02-27 20:45:42 +0530

    Added courseType to schemaTypes/index.ts and updated exports

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,10 +1,13 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { studentType } from "./studentType";
+import { courseType } from "./courseType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     studentType,
+    courseType,
   ],
 }
 
 export * from "./studentType";
+export * from "./courseType";
